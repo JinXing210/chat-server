f8db03d45e74c08f22e49d2e586c0aba0c5fc939
JinXing210 <36975869+JinXing210@users.noreply.github.com>
2018-03-07 17:44:38 +0800

    first project

---
+++ chat-server/socket_server.js
@@ -0,0 +1,50 @@
+/*
+ * (C) Copyright 2018.3 Media Server Administrator
+ */
+
+'use strict';
+var path = require('path');
+
+var ws = require('ws');
+//-----------------------------------------------------------//
+//-----------------------------------------------------------//
+//-----------------------------------------------------------//
+//-----------------------------------------------------------//
+module.exports = function() {
+    var idCounter = 0;
+    function nextUniqueId() {
+        idCounter++;
+        return idCounter.toString();
+    }
+    
+    var idChatCounter = 0;
+    function nextChatId() {
+        idChatCounter++;
+        return idChatCounter.toString();
+    }
+
+    const wss = new WebSocket.Server({ port: 4500 });
+
+    wss.on('connection', function(ws) {
+        var sessionId = nextUniqueId();
+        console.log('Connection received with sessionId ' + sessionId);
+        ws.send(JSON.stringify({
+            id : 'connected',
+            response : 'accepted',
+            sessionId : sessionId
+        }));
+
+        ws.on('error', function(error) {
+            console.log('Connection ' + sessionId + ' error');
+        });
+    
+        ws.on('close', function() {
+            console.log('Connection ' + sessionId + ' closed');
+        });
+    
+        ws.on('message', function(_message) {
+            var message = JSON.parse(_message);
+            console.log('Connection ' + sessionId + ' received message ', message.id);
+        });
+    });
+}
---
+++ m_server.js
@@ -0,0 +1,54 @@
+//-------------------------------------------------------//
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require("fs");
+var	https = require('https');
+//-------------------------------------------------------//
+// var hskey = fs.readFileSync('key.pem');
+// var hscert = fs.readFileSync('cert.pem');
+// var options = {
+//     key: hskey,
+//     cert: hscert
+// };
+//-------------------------------------------------------//
+// Setting environment
+// app.set('views', __dirname + '/client');
+// app.set('view engine', 'ejs');
+// app.engine('html', require('ejs').renderFile);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+// app.use(session({
+//     secret: '@#@$MYSIGN#@$#$',
+//     resave: false,
+//     saveUninitialized: true
+// }));
+// app.use(express.static(__dirname + '/client'));
+//-------------------------------------------------------//
+// Start the app by listening on <port>
+// var port = process.env.PORT || 443;
+var port = process.env.PORT || 443;
+// var port = process.env.PORT || 80;
+//-------------------------------------------------------//
+// var options =
+// {
+//   key:  fs.readFileSync('keys/server.key'),
+//   cert: fs.readFileSync('keys/server.crt')
+// };
+// var options = {
+//   key:  fs.readFileSync('keys/privkey.pem'),
+//   cert: fs.readFileSync('keys/cert.pem')
+// //   ca: fs.readFileSync('keys/fullchain.pem')
+// };
+// var server = app.listen(port, function(){
+//     console.log("Live Video Chat Server has started on port " + port);
+// });
+// var server = https.createServer(options, app).listen(port, function(){
+//     console.log("Live Video Chat Server has started on port " + port);
+// });
+
+require('./chat-server/socket_server');
+//-------------------------------------------------------//
+// RESTful API for client
+//-------------------------------------------------------//
